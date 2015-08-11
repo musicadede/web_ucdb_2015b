bfd2709995f0b6923455787a216f3ab82f4be334
virmerson <virmerson@hightechcursos.com.br>
2015-08-10 22:21:18 -0400

    Estudo de CallBack com Jquery, request GET e request POST

--- src/main/java/webajax/ClienteController.java
+++ src/main/java/webajax/ClienteController.java
@@ -1,6 +1,8 @@
 package webajax;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,10 +13,28 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/cli.do")
 public class ClienteController extends HttpServlet {
 
+	
+	private static List<Cliente> lista = new ArrayList<>();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 
-		resp.getWriter().print("<p> oi do Servlet </p>");
+		
+		resp.getWriter().print( lista.toString()) ;
+	}
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		String nome = req.getParameter("nome");
+		String email = req.getParameter("email");
+		
+		Cliente cliente = new Cliente(nome, email);
+		lista.add(cliente);
+		
+		resp.getWriter().print(lista);
 	}
 }
