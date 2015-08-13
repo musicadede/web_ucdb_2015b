0258c234ee7233abb7c596813252c564533a606d
virmerson <virmerson@hightechcursos.com.br>
2015-08-12 22:29:36 -0400

    Populando caixa de selecao HTML com Jquery e Json Servlet e Jackson

---
+++ src/main/java/webajax/Estado.java
@@ -0,0 +1,34 @@
+package webajax;
+
+/**
+ * Created by Virmerson Bento dos Santos on 12/08/15.
+ */
+public class Estado {
+    private String uf;
+    private String nome;
+
+    public Estado(String uf, String nome) {
+        this.uf=uf;
+        this.nome=nome;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+
+}
---
+++ src/main/java/webajax/EstadoController.java
@@ -0,0 +1,29 @@
+package webajax;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Virmerson Bento dos Santos on 12/08/15.
+ */
+@WebServlet("/estado.do")
+public class EstadoController extends HttpServlet {
+
+    List<Estado> estados = Arrays.asList(new Estado("MS", "Mato Grosso do Sul"), new Estado ("SP", "São Paulo"));
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(estados);
+
+        resp.getWriter().print(json);
+    }
+}
